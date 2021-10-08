adbb16e018150bdd08427cbdd4592f512616c854
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2021-10-08 17:23:31 +0200

    Bug: hide and seek

---
+++ README.md
@@ -0,0 +1,59 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# MY PORTFOLIO
+
+> This project was built for Microverse [under these guidance objectives](https://github.com/microverseinc/curriculum-html-css/blob/main/portfolio/1_setup.md) > _note: you can only access this link if you're a Micronault (Microverse Student)_
+
+## Live Demo
+>   You can access the live demo of this repo [here](https://chimwemwe-127001.github.io/microverse_portfolio_project/)
+
+![gif screenshot](./assets/img/Desktop_Version_Preview.gif)
+
+## How to Setup
+> You can simply clone or download [this repository](https://github.com/Chimwemwe-127001/microverse_portfolio_project), and use your favoright browser or code editor to run this program.
+
+- To open the project after download, simply double click the index.html file
+
+- To open this project using vs code ( for this example) or your favorite code editor, you can follow the guide below:
+> in your cmd or command line navigate to where this project is located, then;
+```cmd
+cd microverse_portfolio_project 
+```
+> thereafter run
+```cmd
+code .
+```
+
+## Built With
+
+- HTML
+- CSS
+
+## Authors
+
+👤 **Chimwemwe Sinyinza**
+
+- GitHub: [@Chimwemwe-127001](https://github.com/Chimwemwe-127001)
+- Twitter: [@chimwewme](https://twitter.com/chimwemwe)
+
+👤 **Awoyemi Femi**
+
+- GitHub: [@phelian23](https://github.com/phelian23)
+
+👤 **Nashaat Shahier**
+
+- GitHub: [@ShahierNashaat](https://github.com/ShahierNashaat)
+
+👤 **Ricky Mormor**
+
+- GitHub: [@cwaku](https://github.com/cwaku)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
---
+++ main.js
@@ -0,0 +1,198 @@
+const toolbar = document.querySelector('.toolbar__2');
+const topCross = document.querySelector('.top');
+const middleCross = document.querySelector('.middle');
+const bottomCross = document.querySelector('.bottom');
+const headline = document.querySelector('.headline');
+const works = document.querySelector('.works');
+const About = document.querySelector('.about-me');
+const form = document.querySelector('.form');
+const footer = document.querySelector('.footer');
+const trigger = document.getElementById('btn-trigger');
+const hideScrollBar = document.querySelector('body');
+const worksData = [
+  6,
+  {
+    coverImg: './assets/img/Img__Plaholder.png',
+    technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+  },
+];
+
+for (let i = 0; i < worksData[0]; i += 1) {
+  const recentWorksTemp = `
+  <div class="img-plaholder">
+  <img src="${worksData[1].coverImg}" alt="Multi-Post Stories Gain+Glory">
+  <div class="txt-content">
+    <h3>Multi-Post Stories<br>+Glory</h3>
+      <ul>
+        <li><p>${worksData[1].technologies[0]}</p></li>
+        <li><p>${worksData[1].technologies[1]}</p></li>
+        <li><p>${worksData[1].technologies[2]}</p></li>
+        <li><p>${worksData[1].technologies[3]}</p></li> 
+      </ul>
+      <a class="btn popup " href="#">See Project</a>
+  </div>
+  `;
+  document.querySelector('.grid-container').innerHTML += recentWorksTemp;
+}
+const popupTriggers = document.querySelectorAll('.popup');
+const popupWindow = document.querySelector('.pop-up');
+const modal = {
+  name: 'Keeping track of hundreds of components',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+  featuredImage: './assets/img/Snapshoot_Portfolio.png',
+  desktopImage: './assets/img/Snapshoot_Portfolio_desktop.png',
+  seeLiveImg: './assets/img/see-live.svg',
+  seeSourceImg: './assets/img/Vector.svg',
+  technologiesMobile: ['Ruby on the rails', 'CSS', 'JavaScript'],
+  technologiesDesktop: ['Codekit', 'Github', 'JavaScript', 'Boostrap', 'Terminal', 'Codepen'],
+  linktoliveversion: '#',
+  linktosource: '#',
+};
+
+function displayPopup() {
+  let popupTemplate = '';
+  if (window.innerWidth < 900) {
+    popupTemplate = `
+      <img class="close" src="./assets/img/Icon.svg" alt="">
+      <img class="mobile" src="./assets/img/Snapshoot_Portfolio.png" alt="">
+      <div class="btnsh1">
+        <h1>${modal.name}</h1>
+        <div class="side">
+          <a href="${modal.linktoliveversion}" class="btn">See Live<img src="./assets/img/see-live.svg" alt=""></a>
+          <a href="${modal.linktosource}" class="btn">See Source<img src="./assets/img/Vector.svg" alt=""></a>
+        </div>
+      </div>
+      <ul>
+        <li><p>${modal.technologiesMobile[0]}</p></li>
+        <li><p>${modal.technologiesMobile[1]}</p></li>
+        <li><p>${modal.technologiesMobile[2]}</p></li>
+      </ul>
+      <p>${modal.description}</p>
+      <div class="btns">
+        <a class="btn">See Live<img src="${modal.seeLiveImg}" alt=""></a>
+        <a class="btn">See Source<img src="${modal.seeSourceImg}" alt=""></a>
+      </div>
+    `;
+  } else {
+    popupTemplate = `
+    <img class="close" src="./assets/img/desktop_modal_close.svg" alt="">
+    <img class="desktop" src="./assets/img/Snapshoot_Portfolio_desktop.png" alt="">
+    <div class="btnsh1">
+      <h1>${modal.name}</h1>
+      <div class="side">
+        <a class="btn">See Live<img src="${modal.seeLiveImg}" alt=""></a>
+        <a class="btn">See Source<img src="${modal.seeSourceImg}" alt=""></a>
+      </div>
+    </div>
+    <ul>
+      <li><p>${modal.technologiesDesktop[0]}</p></li>
+      <li><p>${modal.technologiesDesktop[1]}</p></li>
+      <li><p>${modal.technologiesDesktop[2]}</p></li>
+      <li><p>${modal.technologiesDesktop[3]}</p></li>
+      <li><p>${modal.technologiesDesktop[4]}</p></li>
+      <li><p>${modal.technologiesDesktop[5]}</p></li>
+      <li><p>${modal.technologiesDesktop[6]}</p></li>
+     </ul>
+    <p>${modal.description}</p>
+    `;
+  }
+
+  return popupTemplate;
+}
+
+function toggleNavShow() {
+  toolbar.classList.toggle('show');
+  topCross.classList.toggle('cross');
+  middleCross.classList.toggle('cross');
+  bottomCross.classList.toggle('disappear');
+  headline.classList.toggle('blur');
+  works.classList.toggle('blur');
+  About.classList.toggle('blur');
+  form.classList.toggle('blur');
+  footer.classList.toggle('blur');
+  toolbar.classList.toggle('blur');
+}
+trigger.addEventListener('click', () => {
+  toggleNavShow();
+});
+
+window.addEventListener('hashchange', () => {
+  toggleNavShow();
+});
+
+popupTriggers.forEach((project) => {
+  project.addEventListener('click', (e) => {
+    e.preventDefault();
+    popupWindow.innerHTML = displayPopup();
+    popupWindow.classList.add('showpopup');
+    headline.classList.add('blur');
+    works.classList.add('blur');
+    About.classList.add('blur');
+    form.classList.add('blur');
+    footer.classList.add('blur');
+    toolbar.classList.add('blur');
+    hideScrollBar.classList.add('hide-scrollbar');
+    const popupClose = document.querySelector('.close');
+    popupClose.addEventListener('click', () => {
+      popupWindow.classList.remove('showpopup');
+      headline.classList.remove('blur');
+      works.classList.remove('blur');
+      About.classList.remove('blur');
+      form.classList.remove('blur');
+      footer.classList.remove('blur');
+      toolbar.classList.remove('blur');
+      hideScrollBar.classList.remove('hide-scrollbar');
+    });
+    popupClose.addEventListener('click', () => {
+      popupWindow.classList.remove('showpopup');
+      headline.classList.remove('blur');
+      works.classList.remove('blur');
+      About.classList.remove('blur');
+      form.classList.remove('blur');
+      footer.classList.remove('blur');
+      toolbar.classList.remove('blur');
+      hideScrollBar.classList.remove('hide-scrollbar');
+    });
+  });
+});
+
+const formdata = document.querySelector('.form-data');
+const mail = document.getElementById('mail');
+const errorMessage = document.querySelector('.error-message');
+
+formdata.addEventListener('submit', (e) => {
+  const Regex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+  if (mail.value.match(Regex)) {
+    errorMessage.textContent = '';
+  } else {
+    e.preventDefault();
+    errorMessage.innerHTML = '** FORM NOT SENT <br> ** Invalid Email Address ';
+  }
+});
+
+const getData = (e) => {
+  const Regex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+  if (mail.value.match(Regex)) {
+    const formLocalStorageData = {
+      id: Date.now(),
+      name: document.getElementById('name').value,
+      email: document.getElementById('mail').value,
+      message: document.getElementById('msg').value,
+    };
+
+    localStorage.setItem('formLocalStorageData', JSON.stringify(formLocalStorageData));
+  } else {
+    e.preventDefault();
+  }
+};
+formdata.addEventListener('change', getData);
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('formLocalStorageData') !== null) {
+    let grabforminfo = localStorage.getItem('formLocalStorageData');
+    grabforminfo = JSON.parse(grabforminfo);
+    document.getElementById('name').value = grabforminfo.name;
+    document.getElementById('mail').value = grabforminfo.email;
+    document.getElementById('msg').value = grabforminfo.message;
+  }
+});
